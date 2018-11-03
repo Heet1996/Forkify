820e56f960db356359698660f2429ecc9db77d7b
Heet1996 <heet1885@gmail.com>
2018-11-03 17:10:43 +0530

    Added babel and polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
 var path=require('path');
+var htmlPlugin=require('html-webpack-plugin');
 module.exports={
     entry:'./src/js/index.js',
     output:{
-        path:path.resolve(__dirname,'./dist/js'),
+        path:path.resolve(__dirname,'./dist'),
         filename:'bundle.js'
     },
     devServer:{
         contentBase:'./dist'
+    },
+    plugins:[
+        new htmlPlugin({
+            filename:'index.html',
+            template:'./src/index.html'
+        })
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            }
+        ]
     }
 }
